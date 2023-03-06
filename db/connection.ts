d87cfed0839e4aff1565df7ae6703429d14d1b82
Ian Ayala González <iayalag2000@alumno.ipn.mx>
2023-03-06 04:01:56 -0600

    DB coneccion

--- db/connection.ts
+++ db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const db = new Sequelize("veterinaria", "root", "99310675", {
-  host: "localhost",
+export const db = new Sequelize("heroku_367c3cab9e34b03", "b8b743c26b0c34", "018810c7", {
+  host: "us-cdbr-east-06.cleardb.net",
   dialect: "mysql",
 });
