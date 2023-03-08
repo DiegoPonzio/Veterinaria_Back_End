dd1e9e5fd00a59d1a33bef2e73d1a838072fcadc
Ian Ayala González <84100217+pxel1310@users.noreply.github.com>
2023-03-07 18:58:52 -0600

    Delete Dist

--- dist/routes/rol.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routerRol = void 0;
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const routerRol = (0, express_1.Router)();
-exports.routerRol = routerRol;
-routerRol.get("/", controllers_1.getRoles);
-//# sourceMappingURL=rol.js.map
